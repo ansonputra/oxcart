5d2827a5cad72fde1924f91f5e5198d9baa7cf5a
ansonputra <87323762+ansonputra@users.noreply.github.com>
2021-09-14 16:06:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-echo "My HostName: " . gethostname(); 
-echo "\n"
-echo "This is Oxcart Applications" 
+echo "My HostName: " . gethostname() ,"<br>"; 
+echo "This is Oxcart Applications<br>";
 ?>
